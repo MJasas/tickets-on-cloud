ae9927d0d3bbbf0e9a8aef51093ac583475d079f
orux <evaldaskal@gmail.com>
2016-08-02 13:40:45 +0300

    FrontPageCtrl changes

--- public/angular/controllers/FrontPageCtrl.js
+++ public/angular/controllers/FrontPageCtrl.js
@@ -4,7 +4,7 @@ Main page Controller
 *********************************/
 angular.module('TicketsSupportApp')
     .controller('FrontPageCtrl', function($scope, $http, $location, ticketSrv) {
-        $scope.searchAnswer = function(text) {
+       /* $scope.searchAnswer = function(text) {
             $http.get('/api/watson/answers/'+text)
                 .success(function (response){
                     $scope.allAnswers = response;
@@ -16,11 +16,31 @@ angular.module('TicketsSupportApp')
 
         $scope.checkTicket = function(id) {
             // check existence
+            var intId = parseInt(id);
             console.log(id);
             // let's say it is OK
             $location.path('/ticket/id/'+id);
             console.log(JSON.stringify($location.path()));
+        };
 
-        }; 
+        */
+            $scope.search = function(text){
+                if(isNaN(text)){
+                        $http.get('/api/watson/answers/'+text)
+                            .success(function (response){
+                                $scope.allAnswers = response;
+                            })
+                            .error(function(err){
+                                console.log(err);
+                         });
+                } else {
+                     // check existence
+                    console.log(id);
+                    // let's say it is OK
+                    $location.path('/ticket/id/'+id);
+                    console.log(JSON.stringify($location.path()));
+                }
+            };
+
     });
 
